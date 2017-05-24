24b4ebe3bf68feb9df238acb183bb1dbe2f8a76d
David Immanuel <daweiimmanuel@users.noreply.github.com>
2017-05-24 20:17:43 +0700

    edit localhost to on port

--- app.js
+++ app.js
@@ -235,5 +235,5 @@ function isLoggedIn(req, res, next){
 }
 
 app.listen(3000, process.env.IP, function(){
-    console.log("SERVER IS RUNNING on localhost:3000...");
+    console.log("SERVER IS RUNNING on port 3000...");
 });
